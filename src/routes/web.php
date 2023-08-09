3d9bb8abe43c2c9bb65369e2083fedd63296db1a
unknown <ramiz8work@gmail.com>
2023-08-09 12:04:09 +0530

    updates

--- src/routes/web.php
+++ src/routes/web.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Route;
 use Illuminate\Support\Facades\Auth;
 use App\Http\Controllers\UserController;
 
-Route::get('/',function () {
+Route::get('/login',function () {
     return view('admin/login');
 })->name('login');
 
